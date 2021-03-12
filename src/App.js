3ad47ab5136eec3e54d8183fac8a368cbedcb19d
Bartosz Nogalski <develop@nogalski.dev>
2021-03-12 20:13:19 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> NH Watcher</h1>
+        <p> dummy text</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
